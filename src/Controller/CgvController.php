a13fbb5549f78ac2d5a6e0767945565c60d50be9
Robin Lecullier <lecullier.robin@gmail.com>
2023-11-08 18:21:13 +0100

    generate pdf

--- src/Controller/CgvController.php
+++ src/Controller/CgvController.php
@@ -8,6 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class CgvController extends AbstractController
 {
@@ -20,13 +23,34 @@ class CgvController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Générer le texte des CGV en incorporant le nom de l'entreprise
+            // Générer le PDF avec les CGV en incorporant le nom de l'entreprise
             $companyName = $cgv->getCompanyName();
-            // $cgvText = "Conditions Générales de Vente de " . $companyName;
 
-            return $this->render('cgv/generate.html.twig', [
+            // Vous pouvez utiliser le même contenu que dans votre modèle Twig "generate.html.twig"
+            $htmlContent = $this->renderView('cgv/generate.html.twig', [
                 'companyName' => $companyName,
             ]);
+
+            $options = new Options();
+            $options->set('isHtml5ParserEnabled', true);
+            $options->set('isPhpEnabled', true);
+            $dompdf = new Dompdf($options);
+            $dompdf->loadHtml($htmlContent);
+
+            $dompdf->setPaper('A4', 'portrait');
+            $dompdf->render();
+
+            $pdfContent = $dompdf->output();
+
+            $response = new Response($pdfContent);
+            $disposition = $response->headers->makeDisposition(
+                ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+                'cgv.pdf'
+            );
+            $response->headers->set('Content-Type', 'application/pdf');
+            $response->headers->set('Content-Disposition', $disposition);
+
+            return $response;
         }
 
         return $this->render('cgv/generate_form.html.twig', [
